44b8fb319cbf08482c5cc6345ef620a9fb6a1590
Natsuwind <natsuwind@gmail.com>
2011-04-26 19:20:18 +0800

    add admin page and install script

---
+++ Home/EmiForum/Controllers/AdminController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using EmiForum.Models;
+
+namespace EmiForum.Controllers
+{
+    public class AdminController : Controller
+    {
+        //
+        // GET: /Admin/
+
+        public ActionResult Index()
+        {
+            if (Session["admin"] == null || Session["admin"].ToString() != "admin")
+            {
+                return RedirectToAction("AdminLogin", "Admin");
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+        public ActionResult AdminLogin()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AdminLogin(string username, string password)
+        {
+            if (username.Trim() == "adminywen" && password == "123321aa")
+            {
+                Session["admin"] = "admin";
+                return RedirectToAction("Index", "Admin");
+            }
+            else
+            {
+                return View();
+            }
+        }
+        public ActionResult Logout()
+        {
+            Session.Abandon();
+            return View("Index", "Home");
+        }
+
+        [HttpPost]
+        public ActionResult ExecSql(string SqlScript)
+        {
+            if (SqlScript.Trim() != string.Empty)
+            {
+                try
+                {
+                    Admins.ExecSql(SqlScript);
+                }
+                catch (Exception ex)
+                {
+                    ModelState.AddModelError("", string.Format("执行失败，{0}", ex.Message));
+                }
+            }
+            else
+            {
+                ModelState.AddModelError("", "请输入内容！");
+            }
+            return View("Index");
+        }
+    }
+}
---
+++ Home/EmiForum/Models/Admins.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using Natsuhime.Data;
+using System.Data;
+using System.Data.Common;
+using MySql.Data.MySqlClient;
+
+namespace EmiForum.Models
+{
+    public class Admins
+    {
+        public static int ExecSql(string SqlScript)
+        {
+            return DbHelper.ExecuteNonQuery(SqlScript);
+        }
+    }
+}
