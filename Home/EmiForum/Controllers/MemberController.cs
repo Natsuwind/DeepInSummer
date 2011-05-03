e46d55a1ebaf909c99e90967e76ce0ea74236db4
Natsuwind <natsuwind@gmail.com>
2011-05-03 18:57:37 +0800

    register & login

--- Home/EmiForum/Controllers/MemberController.cs
+++ Home/EmiForum/Controllers/MemberController.cs
@@ -29,14 +29,36 @@ namespace EmiForum.Controllers
         {
             if (ModelState.IsValid)
             {
-                shortUserInfo.LastLoginDate = DateTime.MinValue;
-                shortUserInfo.LastLoginIp = "";
-                shortUserInfo.RegDate = DateTime.Now;
-                shortUserInfo.RegIp = Request.UserHostAddress;
-                shortUserInfo.Salt = "";
-                shortUserInfo.SecQues = "";
-                Users.CreateUser(shortUserInfo);
-                return RedirectToAction("Index", "Home");
+                switch (Users.IsExits(shortUserInfo.Username, shortUserInfo.Email))
+                {
+                    case 0:
+                        shortUserInfo.Password =
+                            System.Web.Security.FormsAuthentication.HashPasswordForStoringInConfigFile(shortUserInfo.Password, "MD5");
+                        shortUserInfo.LastLoginDate = DateTime.MinValue;
+                        shortUserInfo.LastLoginIp = "";
+                        shortUserInfo.RegDate = DateTime.Now;
+                        shortUserInfo.RegIp = Request.UserHostAddress;
+                        shortUserInfo.Salt = "";
+                        shortUserInfo.SecQues = "";
+                        Users.CreateUser(shortUserInfo);
+
+                        Users.SetLoginStatus(shortUserInfo.Email);
+                        return RedirectToAction("Index", "Home");
+                    case 3:
+                        ModelState.AddModelError("", "用户名和邮箱已被其他人注册，请重新填写。（<a href=\"#\">[忘记密码？]</a>）");
+                        return View(shortUserInfo);
+                    case 2:
+                        ModelState.AddModelError("", "邮箱已被其他人注册,请重新填写。（<a href=\"#\">[忘记密码？]</a>）");
+                        return View(shortUserInfo);
+                    case 1:
+                        ModelState.AddModelError("", "用户名已被其他人注册,请重新填写。（<a href=\"#\">[忘记密码？]</a>）");
+                        return View(shortUserInfo);
+                    default:
+                        ModelState.AddModelError("", "注册发生错误！");
+                        return View(shortUserInfo);
+
+                }
+
             }
             else
             {
@@ -44,8 +66,14 @@ namespace EmiForum.Controllers
             }
         }
 
-        [HttpPost]
+
         public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(ShortUserInfo shortUserInfo)
         {
             return View();
         }
--- Home/EmiForum/Models/Users.cs
+++ Home/EmiForum/Models/Users.cs
@@ -28,5 +28,103 @@ namespace EmiForum.Models
 		    };
             DbHelper.ExecuteNonQuery(CommandType.Text, "INSERT INTO members (`username`, `password`, `email`, `regip`, `regdate`, `lastloginip`, `lastlogindate`, `salt`, `secques`) VALUES(?username,?password,?email,?regip,?regdate,?lastloginip,?lastlogindate,?salt,?secques)", prams);
         }
+
+        public static int IsExits(string username, string email)
+        {
+            bool IsExitsUsername = false;
+            bool IsExitsEmail = false;
+            DbParameter[] prams = 
+		    {
+			    DbHelper.MakeInParam("?username", (DbType)MySqlDbType.String, 15,username),
+			    DbHelper.MakeInParam("?email", (DbType)MySqlDbType.String, 32,email)
+		    };
+            IDataReader dr = DbHelper.ExecuteReader(CommandType.Text, "SELECT username,email FROM members WHERE username=?username OR email=?email", prams);
+            while (dr.Read() && !IsExitsUsername && !IsExitsEmail)
+            {
+                if (!IsExitsUsername && dr["username"].ToString().Trim() == username)
+                {
+                    IsExitsUsername = true;
+                }
+                if (!IsExitsEmail && dr["email"].ToString().Trim() == email)
+                {
+                    IsExitsEmail = true;
+                }
+            }
+            dr.Close();
+
+
+            if (IsExitsUsername && IsExitsEmail)
+                return 3;
+            if (IsExitsUsername)
+                return 1;
+            if (IsExitsEmail)
+                return 2;
+            return 0;
+        }
+
+        public static ShortUserInfo GetUserInfo(int uid)
+        {
+            if (uid <= 0)
+            {
+                return null;
+            }
+
+            DbParameter[] prams = 
+                {
+                    DbHelper.MakeInParam("?uid", (DbType)MySqlDbType.Int24, 4,uid)
+                };
+            IDataReader dr = DbHelper.ExecuteReader(CommandType.Text, "SELECT * FROM members WHERE uid=?uid", prams);
+            ShortUserInfo shortUserInfo = BindUserInfo(dr);
+            dr.Close();
+            return shortUserInfo;
+        }
+
+        public static ShortUserInfo GetUserInfo(string email)
+        {
+            if (email.Trim() == string.Empty)
+            {
+                return null;
+            }
+            
+            DbParameter[] prams = 
+                {
+			    DbHelper.MakeInParam("?email", (DbType)MySqlDbType.String, 32,email)
+                };
+            IDataReader dr = DbHelper.ExecuteReader(CommandType.Text, "SELECT * FROM members WHERE email=?email", prams);
+            ShortUserInfo shortUserInfo = BindUserInfo(dr);
+            dr.Close();
+            return shortUserInfo;
+        }
+
+        static ShortUserInfo BindUserInfo(IDataReader dr)
+        {
+            ShortUserInfo shortUserInfo = null;
+            while (dr.Read())
+            {
+                shortUserInfo = new ShortUserInfo();
+                shortUserInfo.Uid = Convert.ToInt32(dr["uid"]);
+                shortUserInfo.Username = dr["username"].ToString();
+                shortUserInfo.Password = dr["password"].ToString();
+                shortUserInfo.Email = dr["email"].ToString();
+                shortUserInfo.RegIp = dr["regip"].ToString();
+                shortUserInfo.RegDate = Convert.ToDateTime(dr["regdate"]);
+                shortUserInfo.LastLoginIp = dr["lastloginip"].ToString();
+                shortUserInfo.LastLoginDate = Convert.ToDateTime(dr["lastlogindate"]);
+                shortUserInfo.Salt = dr["salt"].ToString();
+                shortUserInfo.SecQues = dr["secques"].ToString();
+            }
+            return shortUserInfo;
+        }
+
+        public static void SetLoginStatus(ShortUserInfo shortUserInfo)
+        {
+            HttpContext.Current.Session["login"] = shortUserInfo.Uid;
+        }
+
+        internal static void SetLoginStatus(string email)
+        {
+            ShortUserInfo shortUserInfo = GetUserInfo(email);
+            SetLoginStatus(shortUserInfo);
+        }
     }
 }
